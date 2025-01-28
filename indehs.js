af22b85ee010830c96361ab51f8d6cfddbb38d06
ABDURAHIM <tazidinovabdurahim5@gmail.com>
2025-01-28 10:44:19 +0600

    git status

--- indehs.js
+++ indehs.js
@@ -2,11 +2,9 @@ const passwordInput = document.getElementById('password');
 const togglePassword = document.getElementById('togglePassword');
 
 togglePassword.addEventListener('click', () => {
-  // Паролду көрсөтүү же жашыруу
   const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
   passwordInput.setAttribute('type', type);
 
-  // Көздүн сүрөтүн өзгөртүү
   togglePassword.setAttribute(
     'name',
     type === 'password' ? 'eye-off-outline' : 'eye-outline'
